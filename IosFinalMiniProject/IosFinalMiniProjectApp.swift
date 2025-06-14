ba2eda5b1d564401812e338d972cb9f5178993e3
JiseongKo <kojs1779@gmail.com>
2025-06-14 21:22:01 +0900

    Initial Commit

---
+++ IosFinalMiniProject/IosFinalMiniProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosFinalMiniProjectApp.swift
+//  IosFinalMiniProject
+//
+//  Created by Jiseong Ko on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct IosFinalMiniProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
